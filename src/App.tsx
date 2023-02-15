762433c6342c023d87a26b02a62ab4f2423e46eb
Rokon Zaman <rokon_cse@outlook.com>
2023-02-16 01:01:33 +0600

    Implemented search & filter option

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
-function App() {
-    return <h1>Monster Rolodex</h1>;
+import { Component } from 'react';
+
+interface AppProps {}
+
+interface AppState {
+    monsters: { id: string; name: string }[];
+    searchTerm: string;
+}
+
+class App extends Component<AppProps, AppState> {
+    state: AppState;
+
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { monsters: [], searchTerm: '' };
+    }
+
+    // perfect place for network request
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json()) // response: readable stream
+            .then((users) => {
+                this.setState(
+                    () => ({ monsters: users }),
+                    () => console.log(this.state)
+                );
+            })
+            .catch((err) => console.error(err));
+    }
+
+    render() {
+        const filteredMonsters = this.state.monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(this.state.searchTerm)
+        );
+
+        return (
+            <div>
+                <input
+                    type="search"
+                    placeholder="Search for a Monster"
+                    onChange={(e) => {
+                        const searchTerm = e.target.value.toLowerCase();
+                        this.setState({ searchTerm });
+                    }}
+                />
+                {filteredMonsters.map((monster) => (
+                    <p key={monster.id} style={{ fontSize: 50, color: 'red' }}>
+                        {monster.name}
+                    </p>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
+
