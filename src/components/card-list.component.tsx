e4ba6f0be657af1c89e9d18bbaa6e7d79aebe0bd
Rokon Zaman <rokon_cse@outlook.com>
2023-02-17 01:56:07 +0600

    Refactored CardList component to make functional.

--- src/components/card-list.component.tsx
+++ src/components/card-list.component.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import Card from './card.component';
 import Monster from '../models/monster';
 import './card-list.style.css';
@@ -7,18 +6,14 @@ interface CardListProps {
     monsterList: Monster[];
 }
 
-interface CardListState {}
-
-class CardList extends Component<CardListProps, CardListState> {
-    render() {
-        return (
-            <div className="card-list-container">
-                {this.props.monsterList.map((monster) => (
-                    <Card key={monster.id} monster={monster} />
-                ))}
-            </div>
-        );
-    }
-}
+const CardList = ({ monsterList }: CardListProps) => {
+    return (
+        <div className="card-list-container">
+            {monsterList.map((monster) => (
+                <Card key={monster.id} monster={monster} />
+            ))}
+        </div>
+    );
+};
 
 export default CardList;
