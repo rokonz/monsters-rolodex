eee415892f946ad9ee753390e1cd4b927c0885bd
Rokon Zaman <rokon_cse@outlook.com>
2023-02-17 01:35:06 +0600

    Refactored the App component to a functional component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,46 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list.component';
 import SearchInput from './components/search-input.component';
 import Monster from './models/monster';
 import './App.css';
 
-interface AppProps {}
+function App() {
+    const [monsters, setMonsters] = useState<Monster[]>([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredMonsters, setFilteredMonsters] = useState<Monster[]>([]);
 
-interface AppState {
-    monsters: Monster[];
-    searchTerm: string;
-}
-
-class App extends Component<AppProps, AppState> {
-    state: AppState;
-
-    constructor(props: AppProps) {
-        super(props);
-        this.state = { monsters: [], searchTerm: '' };
-    }
-
-    handleSearchTermChange = (searchTerm: string) => {
-        this.setState({ searchTerm });
-    };
-
-    // perfect place for network request
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json()) // response: readable stream
-            .then((users: Monster[]) => this.setState({ monsters: users }))
+            .then((users: Monster[]) => setMonsters(users))
             .catch((err) => console.error(err));
-    }
+    }, []);
 
-    render() {
-        return (
-            <div className="app">
-                <h1 className="app-title">Monster Rolodex</h1>
-                <SearchInput
-                    placeholder="Search for a Monster"
-                    searchTerm={this.state.searchTerm}
-                    onInputChange={this.handleSearchTermChange}
-                />
-                <CardList monsterList={this.getFilteredMonsters()} />
-            </div>
+    useEffect(() => {
+        // The filtering logic only runs when it is relevent.
+        const newFilteredMonsters = monsters.filter((monster) =>
+            monster.name
+                .toLowerCase()
+                .includes(searchTerm.trimStart().toLowerCase())
         );
-    }
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchTerm]);
 
-    getFilteredMonsters() {
-        const { searchTerm, monsters } = this.state;
-        const filteredMonsters = monsters.filter((monster) =>
-            monster.name.toLowerCase().includes(searchTerm)
-        );
-        return filteredMonsters;
-    }
+    const handleSearchTermChange = (userInput: string) => {
+        setSearchTerm(userInput);
+    };
+
+    return (
+        <div className="app">
+            <h1 className="app-title">Monster Rolodex</h1>
+            <SearchInput
+                placeholder="Search for a Monster"
+                searchTerm={searchTerm}
+                onInputChange={handleSearchTermChange}
+            />
+            <CardList monsterList={filteredMonsters} />
+        </div>
+    );
 }
 
 export default App;
